62f1b84cca9b5800bd1726d4af434cc63a9f58d6
FelicityOdera <87133135+FelicityOdera@users.noreply.github.com>
2022-09-14 11:34:20 +0300

    Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+"""
+a class Square that defines a square by: 4-square.py
+"""
 
 
 class Square:
-    '''Inisialize Class Square
-    '''
-
+    """
+    position should be use by using space when position[1] > 0
+    """
     def __init__(self, size=0, position=(0, 0)):
-        self.size = size
+        self.__size = size
         self.position = position
 
     @property
@@ -14,12 +17,47 @@ class Square:
         return self.__size
 
     @size.setter
-    def size(self, size):
-        if type(size) != int:
+    def size(self, value):
+        self.__size = value
+        if type(self.__size) is not int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        if self.__size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        self.__position = value
+        if not isinstance(self.position, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if len(self.position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(self.position[0]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(self.position[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if self.position[0] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if self.position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
 
     def area(self):
-        return (self.size * self.size)
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size > 0:
+            if self.position[1] > 0:
+                for line in range(self.position[1]):
+                    print("")
+            for i in range(self.size):
+                if self.position[0] > 0:
+                    for white_spaces in range(self.position[0]):
+                        print("#", end="")
+                for j in range(self.size):
+                    print("#", end="")
+                print("")
+        else:
+            print("")
