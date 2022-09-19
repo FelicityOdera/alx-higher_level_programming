87ef2649dff95dd0058e207e86c4110242e1e970
FelicityOdera <87133135+FelicityOdera@users.noreply.github.com>
2022-09-19 12:40:15 +0300

    N queens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,62 +1,134 @@
 #!/usr/bin/python3
-"""
-nqueens backtracking program to print the coordinates of n queens
-on an nxn grid such that they are all in non-attacking positions
+"""Program to compute solutions to the 'N Queens' problem.
+The 'N Queens' problem is to enumerate all the possible arrangements
+of N Queens on an NxN chessboard such that no two pieces lie along
+the same vertical, horizontal, or diagonal. It is difficult to compute
+by brute force because even for relatively small `n` the number of
+arrangements to test grows factorially (N**2 choose N). The solution
+uses backtracking to quickly prune the tree of possible arrangments by
+abandoning unsuccessful branches once a partial solution can be rejected.
+Example:
+    This program can be invoked from the command line by specifying the
+    value for `n`, such as:
+        $ ./nqueens 8
 """
 
 
-from sys import argv
+def nqueens(n, c=[]):
+    """Routine to recursively compute solutions to N queens problem.
+    Recursively computes solutions until all branches of solution
+    tree have been visited. Partial solutions that can be rejected
+    are skipped over.
+    Args:
+        n (int): dimensions of board and number of queens.
+        c (list): list of current candidate positions for solution.
+    Output: Prints valid solutions to stdout.
+    """
+    if reject(n, c):
+        return
+    if accept(n, c):
+        print(c)
+    s = first_candidate(n, c)
+    while s is not None:
+        nqueens(n, s)
+        s = next_candidate(n, s)
+
+
+def reject(n, c):
+    """Subroutine to test whether current partial solution is valid.
+    Tests whether the most recently added position is in conflict
+    with any other positions. Earlier positions are assumed to be
+    valid as they were previously tested against those in the list
+    before them.
+    Args:
+        n (int): dimensions of board and number of queens.
+        c (list): list of current candidate positions for solution.
+    Returns: True if the final position is in conflict with an earlier
+        position, otherwise False.
+    """
+    try:
+        last = c[-1]
+    except IndexError:
+        return False
+    for i in c[:-1]:
+        if i[0] == last[0] or i[1] == last[1]:
+            return True
+        if abs(last[0] - i[0]) == abs(last[1] - i[1]):
+            return True
+    return False
+
+
+def accept(n, c):
+    """Subroutine to test whether current list of positions is a solution.
+    If the current candidate solution has not been rejected and is of
+    length `n`, then it can be assumed to be a valid solution.
+    Args:
+        n (int): dimensions of board and number of queens.
+        c (list): list of current candidate positions for solution.
+    Returns: True if len(c) == n, otherwise False.
+    """
+    if len(c) == n:
+        return True
+    return False
+
+
+def first_candidate(n, c):
+    """Subroutine to add position to the end of the candidate solution.
+    If the previous position was not rejected and the length of the
+    `c` is less than `n`, this function adds the next candidate position
+    following the last position.
+    Args:
+        n (int): dimensions of board and number of queens.
+        c (list): list of current candidate positions for solution.
+    Returns: New list with added position, or None if len(c) == n.
+    """
+    if len(c) == n:
+        return None
+    try:
+        last = c[-1]
+    except IndexError:
+        return [[0, 0]]
+    if last[1] < n-1:
+        return c + [[last[0], last[1]+1]]
+    elif last[0] < n-1:
+        return c + [[last[0]+1, 0]]
+    else:
+        return None
+
+
+def next_candidate(n, s):
+    """Subroutine to update last position of candidate solution.
+    If the previous solution was rejected, the final position is
+    advanced to the next position, unless the end of the board
+    has been reached, in which case this function returns None.
+    Args:
+        n (int): dimensions of board and number of queens.
+        s (list): list of current candidate positions for solution.
+    Returns: New list of candidate positions for solution, or None if
+        at end of board.
+    """
+    last = s[-1]
+    if last[1] < n-1:
+        return s[:-1] + [[last[0], last[1]+1]]
+    elif last[0] < n-1:
+        return s[:-1] + [[last[0]+1, 0]]
+    else:
+        return None
+
 
 if __name__ == "__main__":
-    a = []
-    if len(argv) != 2:
-        print("Usage: nqueens N")
+    import sys
+
+    if len(sys.argv) != 2:
+        print('Usage: nqueens N')
         exit(1)
-    if argv[1].isdigit() is False:
-        print("N must be a number")
+    n = sys.argv[1]
+    if not n.isdigit():
+        print('N must be a number')
         exit(1)
-    n = int(argv[1])
+    n = int(n)
     if n < 4:
-        print("N must be at least 4")
+        print('N must be at least 4')
         exit(1)
-
-    # initialize the answer list
-    for i in range(n):
-        a.append([i, None])
-
-    def already_exists(y):
-        """check that a queen does not already exist in that y value"""
-        for x in range(n):
-            if y == a[x][1]:
-                return True
-        return False
-
-    def reject(x, y):
-        """determines whether or not to reject the solution"""
-        if (already_exists(y)):
-            return False
-        i = 0
-        while(i < x):
-            if abs(a[i][1] - y) == abs(i - x):
-                return False
-            i += 1
-        return True
-
-    def clear_a(x):
-        """clears the answers from the point of failure on"""
-        for i in range(x, n):
-            a[i][1] = None
-
-    def nqueens(x):
-        """recursive backtracking function to find the solution"""
-        for y in range(n):
-            clear_a(x)
-            if reject(x, y):
-                a[x][1] = y
-                if (x == n - 1):  # accepts the solution
-                    print(a)
-                else:
-                    nqueens(x + 1)  # moves on to next x value to continue
-
-    # start the recursive process at x = 0
-    nqueens(0)
+    nqueens(n)
+    exit(0)
