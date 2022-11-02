88ffac0e016b4ba993dbd1f81eefd06c70c7b1a0
FelicityOdera <87133135+FelicityOdera@users.noreply.github.com>
2022-11-02 17:12:12 +0300

    Full mandatory

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,5 @@
+-- rints the full description of the table first_table
+-- from the database hbtn_0c_0 in your MySQL server
+-- The database name will be passed as an argument of the mysql command
+-- Not allowed to use the DESCRIBE or EXPLAIN statements
+SHOW CREATE TABLE first_table;
